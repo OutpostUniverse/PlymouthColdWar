b60cc75ad9ffc71cfd46111ce2ea50607399358e
Hooman <Hooman@7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8>
2010-05-10 04:25:50 +0000

    BugFix: Added missing file resource.h to Plymouth Cold War project. Added OP2Script.rc resource file to project.
    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outpost2/LevelsAndMods/trunk/Levels/PlymouthColdWar@694 7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8

---
+++ resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by OP2Script.rc
+//
+#define IDR_MISSIONTEXT                 102
+#define IDB_PIC                         104
+#define IDD_MISSIONINFO                 907
+#define IDC_TEXTAREA                    1001
+#define IDC_PIC                         -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
